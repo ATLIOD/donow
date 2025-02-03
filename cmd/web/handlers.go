ca27b502bdf422de8570d779182b7c3cd87274d2
ATLIOD <dtaburfict@gmail.com>
2025-02-03 15:58:23 -0600

    now reading rows from database and displaying on the frontend

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,40 +1,74 @@
 package main
 
 import (
+	"context"
 	"donow/models"
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-// PageData struct holds the data passed to the template, including categorized tasks.
-type PageData struct {
-	Todo       []models.Task
-	InProgress []models.Task
-	Complete   []models.Task
-}
-
 func tasks(w http.ResponseWriter, r *http.Request, db *pgxpool.Pool) {
-	tasks := []models.Task{
-		{ID: 1, Title: "Learn Go Templates", Stage: "Completed"},
-		{ID: 2, Title: "Build Todo App", Stage: "In Progress"},
-		{ID: 3, Title: "Write tests", Stage: "To Do"},
+	stmt := "SELECT id, title, stage FROM tasks WHERE user_id = 'c51cc54d-a264-4007-ba6f-bfa3ad7466e4'"
+	// rows = result of statement
+	rows, err := db.Query(context.Background(), stmt)
+	// if row error
+	if err != nil {
+		log.Println("error querying tasks")
+		return
+	}
+	defer rows.Close()
+
+	// empty slice of type snippet
+	tasks := []models.Task{}
+
+	// Next() iterates through elements in a slice
+	for rows.Next() {
+		// empty snippet type
+		t := models.Task{}
+		// same scan as before to put data from row into snippet
+		err = rows.Scan(&t.ID, &t.Title, &t.Stage)
+		if err != nil {
+			return
+		}
+		// append snippet to slice
+		tasks = append(tasks, t)
 	}
 
+	// debugging
+	println("read done")
+	fmt.Printf("%+v\n", tasks)
+
+	if err = rows.Err(); err != nil {
+		return
+	}
+
+	// struct will hold categorized tasks
+	type PageData struct {
+		Todo       []models.Task
+		InProgress []models.Task
+		Complete   []models.Task
+	}
 	// Categorize tasks by status
 	var toDo, inProgress, completed []models.Task
 	for _, task := range tasks {
 		switch task.Stage {
-		case "To Do":
+		case "todo":
 			toDo = append(toDo, task)
-		case "In Progress":
+		case "in progress":
 			inProgress = append(inProgress, task)
-		case "Completed":
+		case "done":
 			completed = append(completed, task)
 		}
 	}
+
+	// debugging
+	log.Printf("To Do: %+v\n", toDo)
+	log.Printf("In Progress: %+v\n", inProgress)
+	log.Printf("Completed: %+v\n", completed)
 
 	// Prepare the data for the template
 	data := PageData{
@@ -43,11 +77,13 @@ func tasks(w http.ResponseWriter, r *http.Request, db *pgxpool.Pool) {
 		Complete:   completed,
 	}
 
+	// parse template to display tasks
 	tmpl, err := template.ParseFiles("./ui/html/tasks.html")
 	if err != nil {
 		http.Error(w, "Error loading template: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
+	// parse template to display tasks
 	err = tmpl.Execute(w, data)
 	if err != nil {
 		http.Error(w, "Error rendering template: "+err.Error(), http.StatusInternalServerError)
