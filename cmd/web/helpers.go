6f3ab873fcd7ed3bf06c50d4400e51723614595b
ATLIOD <60109469+ATLIOD@users.noreply.github.com>
2025-03-03 12:43:05 -0600

    removed isLoggedIn function. can instead of user accoutExists function because it logically does the same thing

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -351,22 +351,6 @@ func getCRSFFromID(userID string, db *pgxpool.Pool) (string, error) {
 	return csrfToken, err
 }
 
-func isLoggedIn(r *http.Request, db *pgxpool.Pool) (bool, error) {
-	// get session from token
-	st, err := r.Cookie("session_token")
-	if err != nil && err != http.ErrNoCookie {
-		return false, errors.New("unable to retrieve token")
-	} else if err == http.ErrNoCookie {
-		return false, nil
-	}
-	// get user id if token exists
-	userID, err := getUserIDFromToken(st.Value, db)
-	if err != nil {
-		return false, fmt.Errorf("error retrieving id: %w", err)
-	}
-	return userID != "", nil
-}
-
 func accountExists(r *http.Request, db *pgxpool.Pool) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
