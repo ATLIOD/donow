d8589b1027ed777e6028e692e25d8ecbf053203a
ATLIOD <60109469+ATLIOD@users.noreply.github.com>
2025-03-26 21:41:31 -0500

    deleted redundant js files

--- ui/js/csrf.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('htmx:configRequest', (event) => {
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-    event.detail.headers['X-CSRF-Token'] = csrfToken;
-});
