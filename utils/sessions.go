7985946d9569ee704494d85f68f760531f45ed25
ATLIOD <60109469+ATLIOD@users.noreply.github.com>
2025-03-31 21:23:13 -0500

    added functions to get user data from request

--- utils/sessions.go
+++ utils/sessions.go
@@ -6,3 +6,17 @@ func CookieExists(r *http.Request, name string) bool {
 	st, err := r.Cookie(name)
 	return err == nil && st.Value != ""
 }
+
+// GetUserAgent returns the User-Agent string from the request
+func GetUserAgent(r *http.Request) string {
+	return r.Header.Get("User-Agent")
+}
+
+// GetIP returns the IP address of the client from the request
+func GetIP(r *http.Request) string {
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip == "" {
+		ip = r.RemoteAddr
+	}
+	return ip
+}
